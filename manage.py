993ba0ef633fd2e3e04574fb4d47ceaed63ed257
w1449550206 <1449550206@qq.com>
2019-12-21 16:52:00 +0800

    修改了manage的功能，增加了数据库迁移的东西

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 from app import app
 from app import db
 from app import Student
 
+migrate = Migrate(app, db)
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
